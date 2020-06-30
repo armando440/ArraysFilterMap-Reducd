621403762c9a68dda6797c2febb529970bb14cec
armando44o <armandovelazco2001@gmail.com>
2020-06-30 16:25:08 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 import Senators from './data/senators'
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.party === "Republican")
 }
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter ( Senator => Senator.party === "Democrat") 
 }
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter ( Senator => Senator.party === "Independent")
 }
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter ( Senator => Senator.person.gender === "male")
 }
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter ( Senator => Senator.person.gender === "female" )
 }
 
 export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter ( Senator => Senator.state === state)
 }
 
 export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.map (senator=> {
+    const senatorObj = {
+      firstName: senator.person.firstname,
+      lastName: senator.person.lastname,
+      party: senator.party,
+      gender: senator.person.gender
+    }
+    return senatorObj;
+  });
+   
 }
 
 export const reducedCount = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.reduce((add, senator)=> {
+    if ( senator.party === 'Republican') add.republican++;
+    else if (senator.party === 'Democrat') add.democrat++;
+    else add.independent ++;
+    return add;
+  }, {republican: 0, democrat: 0, independent: 0,});
 }
 
 
